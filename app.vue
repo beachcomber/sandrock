b55656f8388c021e9eeb2277b43e370c164598e9
Robert Wakeford <robert.wakeford@1066consulting.co.uk>
2025-02-14 09:17:58 +0000

    add decorative border to content

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex flex-col min-h-screen">
         <SiteHeader />
-        <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
+        <main class="my-4 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full
+        rounded-lg border-stone-200 border
+        ">
             <NuxtPage />
         </main>
         <SiteFooter />

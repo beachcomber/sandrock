46b62dfb986f5777d4cfa96ac2be80743edd5267
Robert Wakeford <robert.wakeford@1066consulting.co.uk>
2025-02-14 10:26:45 +0000

    Adjust header colours and footer copyright notic

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -8,11 +8,11 @@ const links = [
 <template>
     <footer class="bg-lime-900 py-8 ">
         <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex flex-row">
-            <p class="flex-1 text-sm text-gray-500">© 2021 Robert Wakeford</p>
+            <p class="flex-1 text-sm text-lime-50">©2025 Sandrock Bends Action Group.</p>
             <div class="flex flex-row justify-center gap-4 mb-4 mt-2">
-                <a v-for="link in links" :key="link.name" :href="link.href" class="text-gray-500 hover:text-white">
+                <!-- <a v-for="link in links" :key="link.name" :href="link.href" class="text-gray-500 hover:text-white">
                     <Icon :name="link.icon" size="1.5rem" class="text-grey-300"/>
-                </a>
+                </a> -->
             </div>
         </div>
     </footer>
--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -18,7 +18,7 @@ const navigation = [
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
                     <DisclosureButton
-                        class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                        class="relative inline-flex items-center justify-center rounded-md p-2 text-lime-50 hover:bg-lime-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                         <span class="absolute -inset-0.5" />
                         <span class="sr-only">Open main menu</span>
                         <Bars3Icon v-if="!open" class="block size-6" aria-hidden="true" />
@@ -34,7 +34,7 @@ const navigation = [
                     <div class="hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
                             <a v-for="item in navigation" :key="item.name" :href="item.href"
-                                :class="[item.current ? 'bg-lime-600 text-white' : 'text-gray-300 hover:bg-lime-500 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
+                                :class="[item.current ? 'bg-lime-600 text-lime-50' : 'text-lime-50 hover:bg-lime-500 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
                                 :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
                         </div>
                     </div>
@@ -44,7 +44,7 @@ const navigation = [
         <DisclosurePanel class="sm:hidden">
             <div class="space-y-1 px-2 pb-3 pt-2">
                 <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href"
-                    :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
+                    :class="[item.current ? 'bg-lime-600 text-lime-50' : 'text-lime-50 hover:bg-lime-600 hover:text-lime-50', 'block rounded-md px-3 py-2 text-base font-medium']"
                     :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
             </div>
         </DisclosurePanel>

d3999d574376ce93e4e81b172931ccdcbdc6268b
Robert Wakeford <robert.wakeford@1066consulting.co.uk>
2025-02-14 09:00:13 +0000

    temp remove tags from wildlife pages

--- pages/wildlife/[...slug].vue
+++ pages/wildlife/[...slug].vue
@@ -13,11 +13,11 @@ const { data } = await useAsyncData(`blog-article`, () => {
             <h1 class="text-3xl my-8 text-lime-900">{{ data.title }}</h1>
             <ContentRendererMarkdown :value="data" class="my-8" />
         </ContentRenderer>
-        <div class="my-8">
+        <!-- <div class="my-8">
             <a v-for="tag in data.tags " :key="tag" :href="`/blog/tags/${tag}`"
                 class="text-sm font-semibold inline-block py-2 px-4 rounded-lg text-gray-100 bg-blue-500 uppercase last:mr-0 mr-4">
                 <Icon name="pajamas:label" size="1.5rem" class="text-gray-100 mr-2" /> {{ tag }}
             </a>
-        </div>
+        </div> -->
     </div>
 </template>

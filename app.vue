5b7b219109db6f6e101cf085a189173a8d047d7a
Robert Wakeford <robert.wakeford@1066consulting.co.uk>
2025-02-20 09:08:46 +0000

    fix full hight bug

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
         <!-- <main class="my-4 mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full
         rounded-lg border-stone-200 border
         "> -->
-            <main class="container mx-auto sm:px-6 lg:px-8">
+            <main class="container mx-auto sm:px-6 lg:px-8 grow items-start w-full">
                 <NuxtPage />
             </main>
             <SiteFooter />

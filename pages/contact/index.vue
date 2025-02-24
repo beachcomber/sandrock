63665cc4726b2c51916ca0d4587c1c174304d973
Robert Wakeford <robert.wakeford@1066consulting.co.uk>
2025-02-24 13:00:27 +0000

    add honeypot and bot challenge to form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -9,7 +9,9 @@ const { data } = await useAsyncData('page-data', () => queryContent(path).findOn
             <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
                 <div class="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
                     <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
-                        <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden ring-1 ring-gray-900/10 lg:w-1/2"></div>
+                        <div
+                            class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden ring-1 ring-gray-900/10 lg:w-1/2">
+                        </div>
                         <h2 class="text-pretty text-4xl font-semibold tracking-tight text-lime-900 sm:text-5xl">Get
                             in <span class="text-lime-600">touch</span></h2>
                         <p class="mt-6 text-lg/8 text-gray-600">We are very keen for people to join in our campaign to
@@ -47,16 +49,26 @@ const { data } = await useAsyncData('page-data', () => queryContent(path).findOn
                         </dl>
                     </div>
                 </div>
-                <form name="contact" method="POST" data-netlify="true"
-                    class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48" netlify>
+                <form 
+                    name="contact" 
+                    method="POST" 
+                    data-netlify="true" 
+                    netlify-honeypot="bot-field"
+                    data-netlify-recaptcha="true" 
+                    class="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48">
                     <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
                         <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                             <div>
+                                <p class="hidden">
+                                    <label>
+                                        Don’t fill this out if you’re human: <input name="bot-field" />
+                                    </label>
+                                </p>
                                 <label for="first-name" class="block text-sm/6 font-semibold text-lime-900">First
                                     name</label>
                                 <div class="mt-2.5">
                                     <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
                                 </div>
                             </div>
                             <div>
@@ -64,14 +76,14 @@ const { data } = await useAsyncData('page-data', () => queryContent(path).findOn
                                     name</label>
                                 <div class="mt-2.5">
                                     <input type="text" name="last-name" id="last-name" autocomplete="family-name"
-                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
                                 </div>
                             </div>
                             <div class="sm:col-span-2">
                                 <label for="email" class="block text-sm/6 font-semibold text-lime-900">Email</label>
                                 <div class="mt-2.5">
                                     <input type="email" name="email" id="email" autocomplete="email"
-                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
                                 </div>
                             </div>
                             <div class="sm:col-span-2">
@@ -79,20 +91,21 @@ const { data } = await useAsyncData('page-data', () => queryContent(path).findOn
                                     number</label>
                                 <div class="mt-2.5">
                                     <input type="tel" name="phone-number" id="phone-number" autocomplete="tel"
-                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
                                 </div>
                             </div>
                             <div class="sm:col-span-2">
                                 <label for="message" class="block text-sm/6 font-semibold text-lime-900">Message</label>
                                 <div class="mt-2.5">
                                     <textarea name="message" id="message" rows="4"
-                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                                        class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-lime-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-lime-600" />
                                 </div>
                             </div>
+                            <div data-netlify-recaptcha="true"></div>
                         </div>
                         <div class="mt-8 flex justify-end">
                             <button type="submit"
-                                class="rounded-md bg-lime-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send
+                                class="rounded-md bg-lime-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-600">Send
                                 message</button>
                         </div>
                     </div>
